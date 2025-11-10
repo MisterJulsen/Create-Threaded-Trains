17de9021e1062194e3882cdde72e853678ecfb98
MrJulsen <geisterdrache02@gmail.com>
2025-11-10 22:13:33 +0100

    Fixed fabric version

--- common/src/main/java/de/mrjulsen/ctt/mixin/CommonEventsMixin.java
+++ common/src/main/java/de/mrjulsen/ctt/mixin/CommonEventsMixin.java
@@ -7,13 +7,21 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import com.simibubi.create.content.trains.GlobalRailwayManager;
 import com.simibubi.create.foundation.events.CommonEvents;
 
+import dev.architectury.injectables.annotations.PlatformOnly;
 import net.minecraft.world.level.Level;
 
 @Mixin(CommonEvents.class)
 public class CommonEventsMixin {
 
+    @PlatformOnly(PlatformOnly.FORGE)
     @Redirect(method = "onServerWorldTick", remap = false, at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/GlobalRailwayManager;tick(Lnet/minecraft/world/level/Level;)V", remap = false))
-    private static void disableGlobalManagerTick(GlobalRailwayManager manager, Level level) {
+    private static void disableGlobalManagerTickForge(GlobalRailwayManager manager, Level level) {
+        // Disable default behaviour
+	}
+    
+    @PlatformOnly(PlatformOnly.FABRIC)
+    @Redirect(method = "onWorldTick", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/GlobalRailwayManager;tick(Lnet/minecraft/world/level/Level;)V"))
+    private static void disableGlobalManagerTickFabric(GlobalRailwayManager manager, Level level) {
         // Disable default behaviour
 	}
 }

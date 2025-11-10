a5f4dbe0a3f563795921da9d75ceb049fdf456d6
MrJulsen <geisterdrache02@gmail.com>
2025-11-10 23:24:38 +0100

    Fixed mappings and mixins

--- common/src/main/java/de/mrjulsen/ctt/mixin/CarriageMixin.java
+++ common/src/main/java/de/mrjulsen/ctt/mixin/CarriageMixin.java
@@ -7,13 +7,23 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import com.simibubi.create.content.trains.entity.Carriage;
 
 import de.mrjulsen.ctt.CreateThreadedTrains;
+import dev.architectury.injectables.annotations.PlatformOnly;
 import net.minecraft.world.level.Level;
 
 @Mixin(Carriage.class)
 public class CarriageMixin {
 
+    @PlatformOnly(PlatformOnly.FORGE)
     @Redirect(method = "travel", remap = false, at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/entity/Carriage;manageEntities(Lnet/minecraft/world/level/Level;)V", remap = false))
-    public void onTravel(Carriage carriage, Level level) {
+    public void onTravelForge(Carriage carriage, Level level) {
+        CreateThreadedTrains.getServer().ifPresent(x -> x.execute(() -> {
+            carriage.manageEntities(level);
+        }));
+    }
+    
+    @PlatformOnly(PlatformOnly.FABRIC)
+    @Redirect(method = "travel", remap = false, at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/entity/Carriage;manageEntities(Lnet/minecraft/class_1937;)V", remap = false))
+    public void onTravelFabric(Carriage carriage, Level level) {
         CreateThreadedTrains.getServer().ifPresent(x -> x.execute(() -> {
             carriage.manageEntities(level);
         }));

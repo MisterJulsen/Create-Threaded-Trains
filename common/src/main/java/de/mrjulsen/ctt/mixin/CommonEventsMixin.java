3132d5ad62c040b488a718bd0d6e6859e7bd606d
MrJulsen <geisterdrache02@gmail.com>
2025-11-11 00:05:58 +0100

    Fixed mixins

--- common/src/main/java/de/mrjulsen/ctt/mixin/CommonEventsMixin.java
+++ common/src/main/java/de/mrjulsen/ctt/mixin/CommonEventsMixin.java
@@ -7,13 +7,22 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import com.simibubi.create.content.trains.GlobalRailwayManager;
 import com.simibubi.create.foundation.events.CommonEvents;
 
+import dev.architectury.injectables.annotations.PlatformOnly;
 import net.minecraft.world.level.Level;
 
 @Mixin(CommonEvents.class)
 public class CommonEventsMixin {
 
+    @PlatformOnly(PlatformOnly.FORGE)
     @Redirect(method = "onServerWorldTick", remap = false, at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/GlobalRailwayManager;tick(Lnet/minecraft/world/level/Level;)V", remap = false))
     private static void disableGlobalManagerTickForge(GlobalRailwayManager manager, Level level) {
+        // Disable default behaviour
+	}
+
+    
+    @PlatformOnly(PlatformOnly.FABRIC)
+    @Redirect(method = "onServerWorldTick", remap = false, at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/trains/GlobalRailwayManager;tick(Lnet/minecraft/class_1937;)V", remap = false))
+    private static void disableGlobalManagerTickFabric(GlobalRailwayManager manager, Level level) {
         // Disable default behaviour
 	}
 }
